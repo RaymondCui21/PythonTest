13df37b5c7c646b86d966281220b7ecc413115fb
Raymond CUI <31235714+RaymondCui21@users.noreply.github.com>
2020-12-03 12:12:08 +1100

    Update boardgame.py
    improved

--- boardgame.py
+++ boardgame.py
@@ -1,101 +1,72 @@
-
-def input_commands(filename):
-    commands = []
-    inputFile = open(filename, "r")
-    for rawCommand in inputFile.readlines():
-        if rawCommand.isspace():
-            continue
-        else:
-            commands.append(rawCommand.strip().upper())
-    inputFile.close()
-    return commands
-
-
 class Robot:
-    def __init__(self, n, m, command):
+    def __init__(self, n, m):
         self.n = n
         self.m = m
-        initposition = command.split()
-        self.x = int(initposition[1])
-        self.y = int(initposition[2])
-        self.direction = initposition[3]
+        self.x = -1
+        self.y = -1
+        self.direction = ''
         self.directions = ('WEST', 'NORTH', 'EAST', 'SOUTH')
-        self.expectx = self.x
-        self.expecty = self.y
+
+    def isValidPosition(self, x, y):
+        return ( 0 <= x <= self.n) and ( 0< y <= self.m)
 
     def action(self, command):
-        command = command.split()
         if command[0] == 'MOVE':
-            if self.direction == 'WEST':
-                self.expectx = self.expectx - 1
-            elif self.direction == 'NORTH':
-                self.expecty = self.expecty + 1
-            elif self.direction == 'EAST':
-                self.expectx = self.expectx + 1
-            elif self.direction == 'SOUTH':
-                self.expecty = self.expecty - 1
-            else:
-                pass
+            self.move()
         elif command[0] == 'LEFT':
             self.direction = self.directions[self.directions.index(self.direction) - 1]
         elif command[0] == 'RIGHT':
             self.direction = self.directions[self.directions.index(self.direction) + 1]
-        elif command[0] == 'PLACE' and command[1].isdecimal() and command[2].isdecimal() and (
-                    command[3] in self.directions):
-            self.x = command[1]
-            self.y = command[2]
-            self.direction = command[3]
+        elif command[0] == 'PLACE':
+            self.place(command)
         elif command[0] == 'REPORT':
             print("X: {} Y: {} Direction: {}".format(self.x, self.y, self.direction))
-        else:
-            pass
 
-        if self.expectx in range(0, self.n) and self.expecty in range(0, self.m):
-            self.x = self.expectx
-            self.y = self.expecty
-        else:
-            pass
+    def move(self):
+        expectx = self.x
+        expecty = self.y
+        if self.direction == 'WEST':
+            expectx = expectx -1
+        elif self.direction == 'NORTH':
+            expecty = expecty + 1
+        elif self.direction == 'EAST':
+            expectx = expectx + 1
+        elif self.direction == 'SOUTH':
+            expecty = expecty - 1
+        if self.isValidPosition(expectx, expecty):
+            self.x = expectx
+            self.y = expecty
 
+    def place(self, command):
+        if self.isValidPlace(command):
+            self.x = int(command[1])
+            self.y = int(command[2])
+            self.direction = command[3]
 
-class Board:
-    def __init__(self, n, m, commands):
-        self.n = n
-        self.m = m
-        self.directions = ('WEST', 'NORTH', 'EAST', 'SOUTH')
-        self.commands = commands
-
-    def initrobot(self):
-        result = -1
-        for commandLine in self.commands:
-            # print(commandLine)
-            command = commandLine.split()
-            if command[0] == 'PLACE' and command[1].isdecimal() and command[2].isdecimal() and (
-                    command[3] in self.directions):
-                if int(command[1]) in range(0, self.n) and int(command[2]) in range(0, self.m):
-                    result = self.commands.index(commandLine)
-                    break
-            else:
-                continue
-        if result == -1:
-            print('Did not find available Place commands.')
-        else:
-            pass
-        return result
+    def isValidPlace(self, command):
+        if command[1].isdecimal() and command[2].isdecimal() and (command[3] in self.directions):
+            if (int(command[1]) <= self.n) and (int(command[2]) <= self.m):
+                return True
 
 
 def main():
-    # text file name is "testcommands.txt"
-    commands = input_commands("testcommands.txt")
-    # assume n and m are 10
-    n = 10
-    m = 10
-    board = Board(n, m, commands)
-    index = board.initrobot()
-    if index != -1:
-        robot = Robot(n, m, commands[index])
-        if index+1 <= len(commands):
-            for command in commands[index+1:]:
+    n = m = 10  # assume n and m are 5
+    robot = Robot(n, m)
+    filename = "testcommands.txt"  # text file name is "testcommands.txt"
+    inputFile = open(filename, "r")
+    command = []
+    for rawCommand in inputFile.readlines():
+        if not rawCommand.isspace():
+            command = rawCommand.strip().upper().split()
+            if robot.direction == '' and (command[0] == 'PLACE'):
+                robot.place(command)
+            elif robot.direction != '':
                 robot.action(command)
+            else:
+                continue
+        else:
+            continue
+    inputFile.close()
 
 
 if __name__ == "__main__":
